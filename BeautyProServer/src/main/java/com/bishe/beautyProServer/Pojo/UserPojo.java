e22e9fbc7e7723387bb721e6411d16b7aecc4100
zhanglei <510745870@qq.com>
2018-12-14 10:01:05 +0800

    修改userpojo

--- BeautyProServer/src/main/java/com/bishe/beautyProServer/Pojo/UserPojo.java
+++ BeautyProServer/src/main/java/com/bishe/beautyProServer/Pojo/UserPojo.java
@@ -1,16 +1,10 @@
 package com.bishe.beautyProServer.Pojo;
 
-import java.util.Date;
-
 public class UserPojo extends BasePojo{
-	private Integer id;
 	private String nickname;
 	private String password;
 	private String email;
 	private String phonenum;
-	/*private Date createDate;
-	private Integer updateUser;
-	private Date updateDate;*/
 	public String getNickname() {
 		return nickname;
 	}
@@ -35,30 +29,4 @@ public class UserPojo extends BasePojo{
 	public void setPhonenum(String phonenum) {
 		this.phonenum = phonenum;
 	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	/*public Date getCreateDate() {
-		return createDate;
-	}
-	public void setCreateDate(Date createDate) {
-		this.createDate = createDate;
-	}
-	public Integer getUpdateUser() {
-		return updateUser;
-	}
-	public void setUpdateUser(Integer updateUser) {
-		this.updateUser = updateUser;
-	}
-	public Date getUpdateDate() {
-		return updateDate;
-	}
-	public void setUpdateDate(Date updateDate) {
-		this.updateDate = updateDate;
-	}*/
-	
-	
 }
