642a0a57f5ea9d561145502e15f543156d389822
zhanglei <510745870@qq.com>
2019-01-03 14:40:37 +0800

    obj转型赋值

--- BeautyProServer/src/main/java/com/bishe/beautyProServer/Controller/RedisController.java
+++ BeautyProServer/src/main/java/com/bishe/beautyProServer/Controller/RedisController.java
@@ -1,28 +1,31 @@
 package com.bishe.beautyProServer.Controller;
 
+import javax.annotation.Resource;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.bishe.beautyProServer.Pojo.UserPojo;
 
 @Controller
 @RequestMapping("/api/v1")
 public class RedisController {
-	@Autowired
-	private RedisTemplate<String, Object> redisTemplate;
+	@Resource
+	protected RedisTemplate<String, Object> redisTemplate;
 	
 	@RequestMapping(value="/redis",method= {RequestMethod.GET})
+	@ResponseBody
 	public Object getRedis(String key,String cookieName,HttpServletRequest request) {
 		Object obj = null;
 		Cookie[] cookies = request.getCookies();
 		if(cookies.length>0) {
 			for (Cookie cookie : cookies) {
 				if(cookie.getName().equals(cookieName)) {
-					redisTemplate.opsForHash().get(key, cookieName);
+					obj=(UserPojo)redisTemplate.opsForHash().get(key, cookie.getValue());
 					break;
 				}
 			}
