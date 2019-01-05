5f4a9d64e1c52c43d630c040324a60813a1049c5
zhanglei <510745870@qq.com>
2019-01-05 17:53:00 +0800

    清除cookie

--- BeautyProServer/src/main/java/com/bishe/beautyProServer/Controller/UserController.java
+++ BeautyProServer/src/main/java/com/bishe/beautyProServer/Controller/UserController.java
@@ -48,13 +48,39 @@ public class UserController {
 			UserPojo user = userService.selectUser(map);
 			if(user!=null) {
 				Cookie cookie=new Cookie("userid", user.getId().toString());
-				/*cookie.setDomain("http://127.0.0.1:8081");
-				cookie.setPath("/");*/
+				/*cookie.setDomain("http://127.0.0.1:8081");*/
+				cookie.setPath("/");
 				cookie.setMaxAge(1000*60*60);
 			    response.addCookie(cookie);
 				redisTemplate.opsForHash().put(OBJECT_KEY, user.getId().toString(), user);
 			}
 			return user;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="/logout",method= {RequestMethod.GET})
+	public int logout(HttpServletRequest request,HttpServletResponse response) {
+		Cookie[] cookies = request.getCookies();
+		if(cookies.length>0) {
+			int count=0;
+			for(int i=0;i<cookies.length;i++) {
+				if(cookies[i]!=null&&cookies[i].getName().equals("userid")) {
+					count++;
+					redisTemplate.opsForHash().delete(OBJECT_KEY, cookies[i].getValue());
+					cookies[i].setMaxAge(0);
+					cookies[i].setPath("/");
+					cookies[i].setValue("");
+		            response.addCookie(cookies[i]); 
+					break;
+				}
+			}
+			if(count>0) {
+				return 1;//注销成功
+			}
+			return 0;//存在其他网站的cookie,您还没有登录当前网站
+		}else {
+			return 0;//您还没有登录
+		}
 	}
 	
 	@ResponseBody
