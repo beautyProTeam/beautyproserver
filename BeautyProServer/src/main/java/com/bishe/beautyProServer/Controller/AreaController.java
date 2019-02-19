9872a4ab7f235c7cbf0f124427ab99fe663dec43
zhanglei <510745870@qq.com>
2019-02-19 16:49:32 +0800

    ...

--- BeautyProServer/src/main/java/com/bishe/beautyProServer/Controller/AreaController.java
+++ BeautyProServer/src/main/java/com/bishe/beautyProServer/Controller/AreaController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bishe.beautyProServer.Pojo.AreaPojo;
@@ -22,7 +23,7 @@ public class AreaController {
 	
 	@PostMapping("/area")
 	@ResponseBody
-	public List<AreaPojo> selectAreaList(Map map) {
+	public List<AreaPojo> selectAreaList(@RequestParam Map<String,Object> map) {
 		List<AreaPojo> areas = areaService.selectAreaList(map);
 		return areas;
 	}
